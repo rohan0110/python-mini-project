ad5d76437e1b8c54b0dd66998792195480c77a3c
rohan gunge <321rohan0011@dbit.in>
2023-03-09 00:24:51 +0530

    9/3/23

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
-    $server="127.0.0.1";
-    $username="localhost@root";
+/*
+    $server="localhost";
+    $username="root";
     $password="";
 
-    $con= mysqli_connect($server,$username,$password);
+    this->$con= mysqli_connect($server,$username,$password);
 
-    if(!=$con){
+    if(!this->$con){
         die("connection failed due to" .mysqli_connect_error());
     }
     echo"success";
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql = "Insert into 'user'('name','username','password','email') values ('$name','$username','$password','$email');";
+    echo $sql;
+
+    if($con->query($sql)==true){
+        echo "successfully inserted";
+    }
+    else{
+        echo"ERROR : "<br>$con->error;
+    }
+*/
 ?>
